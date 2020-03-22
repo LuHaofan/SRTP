4a3b02561f736eabb8d29fcb5f95a178322dc723
Lu Haofan <haofan.17@intl.zju.edu.cn>
2020-03-22 10:12:59 -0500

    updata data

--- Code/ML predict P/test_shape.py
+++ Code/ML predict P/test_shape.py
@@ -24,6 +24,7 @@ p_scaler = joblib.load(pscalername)
 f = 0.046181411
 h = 0.49
 r = 0.3
+'''
 l = np.sqrt(h**2+r**2)
 S = np.pi*r**2+np.pi*r*l
 V = (1/3)*r**2*np.pi*h
@@ -34,9 +35,11 @@ for i in range(10):
     predp = p_model[i].predict(p_var)
     pavg.append(predp[0])
 p = sum(pavg)/10
+'''
+p = h/r
 Kp = [0.2, 0.5, 1, 2, 5, 10, 20]
 Km = Kp
-filename = 'cone.csv'
+filename = 'cone_rough_p.csv'
 with open(filename, 'w', newline='') as file: 
     f_csv = csv.writer(file)
     rows = []
@@ -54,6 +57,7 @@ b = 0.3
 c = 0.3
 V = (4/3)*np.pi*a*b*c
 f = V
+'''
 e2 = 1-c**2/a**2
 S = 2*np.pi*a**2*(1+(c**2*np.arctanh(e2**0.5))/(e2**0.5*a**2))
 AvgR = (3*V/np.pi)**(1/3)
@@ -63,9 +67,11 @@ for i in range(10):
     predp = p_model[i].predict(p_var)
     pavg.append(predp[0])
 p = sum(pavg)/10
+'''
+p = c/a
 Kp = [0.2, 0.5, 1, 2, 5, 10, 20]
 Km = Kp
-filename = 'ellipsoid.csv'
+filename = 'ellipsoid_rough_p.csv'
 with open(filename, 'w', newline='') as file: 
     f_csv = csv.writer(file)
     rows = []
@@ -84,6 +90,7 @@ r = 0.19
 V = 2*np.pi**2*R*r**2
 S = 4*np.pi**2*R*r
 f = 0.213775624
+'''
 AvgR = (3*V/np.pi)**(1/3)
 p_var = p_scaler.transform([[V, S, AvgR]])
 pavg = []
@@ -91,9 +98,11 @@ for i in range(10):
     predp = p_model[i].predict(p_var)
     pavg.append(predp[0])
 p = sum(pavg)/10
+'''
+p = R/r
 Kp = [0.2, 0.5, 1, 2, 5, 10, 20]
 Km = Kp
-filename = 'donut.csv'
+filename = 'donut_rough_p.csv'
 with open(filename, 'w', newline='') as file: 
     f_csv = csv.writer(file)
     rows = []
